876bfb6e12c4390af8e0744b8c48f73c12220891
HJY <1365542967@qq.com>
2022-11-01 23:16:36 +0800

    feat:简单的优雅关闭

--- main.go
+++ main.go
@@ -2,38 +2,76 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"time"
 )
 
-func ListenAndServe(address string) {
+// handler 应用服务器的抽象
+type Handler interface {
+	Handle(ctx context.Context, conn net.Conn)
+	Close() error
+}
+
+// 监听并提供服务，并在收到 closeChan 发来的关闭通知后关闭
+func ListenAndServe(address string, closeCh <-chan struct{}) {
+	log.Println("listen starting ...")
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
-		panic(err)
+		log.Println(err)
+		return
 	}
-	defer listener.Close()
-	log.Println("listen:", address)
+	defer func() {
+		log.Println("listener closing2 ...:")
+		listener.Close()
+		log.Println("listener closed2!")
+	}()
+
+
+
+	// 监听通知
+	go func(closeCh <-chan struct{}) {
+		<-closeCh
+		log.Println("listener closing ...:")
+		listener.Close()
+		log.Println("listener closed!")
+	}(closeCh)
+
 	for {
+		fmt.Println("listener accepting ...")
 		// 监听请求
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Panicln(err)
 			continue
 		}
+		fmt.Println("listener accepted!")
 		// 处理请求
-		go Handler(conn)
+		go Handle(conn)
+
 	}
 }
 
-func Handler(conn net.Conn) {
+func Handle(conn net.Conn) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("发送了panic")
+		}
+	}()
+
+	fmt.Println("listener handing ...")
 	r := bufio.NewReader(conn)
+
+	//panic(8)
 
 	for {
 		data, err := r.ReadBytes('\n')
 		if err != nil {
 			if err == io.EOF {
-				log.Printf("conn :%s closed.", conn.LocalAddr().String())
+				fmt.Println("listener exit!")
 			} else {
 				log.Println("Handler err:", err)
 			}
@@ -46,5 +84,9 @@ func Handler(conn net.Conn) {
 }
 
 func main() {
-	ListenAndServe(":8080")
+	ch := make(chan struct{})
+	go ListenAndServe(":8080", ch)
+	time.Sleep(15 * time.Second)
+	fmt.Println("发送关闭listener信号 ...")
+	ch <- struct{}{}
 }
